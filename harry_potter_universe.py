1b731baab09b2914047c8efdb6ac719072a7f1ee
zotroneneis <popkes@gmx.net>
2018-08-09 11:13:19 -0700

    removed keyword arguments

--- harry_potter_universe.py
+++ harry_potter_universe.py
@@ -70,19 +70,19 @@ class Professor(HogwartsMember):
 
     @classmethod
     def mcgonagall(cls):
-        return cls('Minerva McGonagall', 1935, 'female', 'Transfiguration', house='Griffindor')
+        return cls('Minerva McGonagall', 1935, 'female', 'Transfiguration', 'Griffindor')
 
     @classmethod
     def snape(cls):
-        return cls('Severus Snape', 1960, 'male', 'Potions', house='Slytherin')
+        return cls('Severus Snape', 1960, 'male', 'Potions', 'Slytherin')
 
     @classmethod
     def sprout(cls):
-        return cls('Pomona Sprout', 1931, 'female', 'Herbology', house='Hufflepuff')
+        return cls('Pomona Sprout', 1931, 'female', 'Herbology', 'Hufflepuff')
 
     @classmethod
     def flitwick(cls):
-        return cls('Filius Flitwick', 1958, 'male', 'Charms', house='Ravenclaw')
+        return cls('Filius Flitwick', 1958, 'male', 'Charms', 'Ravenclaw')
 
     def __repr__(self):
         return (f"{self.__class__.__name__}({self._name}, "
@@ -159,19 +159,19 @@ class Pupil(HogwartsMember):
 
     @classmethod
     def harry(cls):
-        return cls('Harry James Potter', 1980, 'male', 'Griffindor', start_year=1991, pet=('Hedwig', 'owl'))
+        return cls('Harry James Potter', 1980, 'male', 'Griffindor', 1991, ('Hedwig', 'owl'))
 
     @classmethod
     def ron(cls):
-        return cls('Ronald Bilius Weasley', 1980, 'male', 'Griffindor', 1991, pet=('Pigwidgeon', 'owl'))
+        return cls('Ronald Bilius Weasley', 1980, 'male', 'Griffindor', 1991, ('Pigwidgeon', 'owl'))
 
     @classmethod
     def hermione(cls):
-        return cls('Hermione Jean Granger', 1979, 'female', 'Griffindor', 1991, pet=('Crookshanks', 'cat'))
+        return cls('Hermione Jean Granger', 1979, 'female', 'Griffindor', 1991, ('Crookshanks', 'cat'))
 
     @classmethod
     def malfoy(cls):
-        return cls('Draco Lucius Malfoy', 1980, 'male', 'Slytherin', 1991, pet=('Unnamed', 'owl') )
+        return cls('Draco Lucius Malfoy', 1980, 'male', 'Slytherin', 1991, ('Unnamed', 'owl') )
 
     @property
     def current_year(self):
@@ -329,11 +329,11 @@ class Charm(Spell):
 
     @classmethod
     def lumos(cls):
-        return cls('Lumos', 'Lumos', 'Illuminates the wand tip', 'simple', min_year=5)
+        return cls('Lumos', 'Lumos', 'Illuminates the wand tip', 'simple', 5)
 
     @classmethod
     def wingardium_leviosa(cls):
-        return cls('Wingardium Leviosa', 'Wingardium Leviosa', 'Makes objects fly', 'simple', min_year=1)
+        return cls('Wingardium Leviosa', 'Wingardium Leviosa', 'Makes objects fly', 'simple', 1)
 
 
 class Transfiguration(Spell):

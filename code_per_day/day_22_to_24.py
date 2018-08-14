f58fa99cdcfd21355dc6d6e86dd4129a9102e106
zotroneneis <popkes@gmx.net>
2018-08-14 08:14:17 -0700

    code day 22 to 24 added

---
+++ code_per_day/day_22_to_24.py
@@ -0,0 +1,80 @@
+class HogwartsMember:
+    """
+    Creates a member of the Hogwarts School of Witchcraft and Wizardry
+    """
+
+    def __init__(self, name: str, birthyear: int, sex: str):
+        self._name = name
+        self.birthyear = birthyear
+        self.sex = sex
+        self._traits = {}
+
+    def write_letter(self, recipient, content):
+        letter_name = f"dear_{recipient}.txt"
+        with Letter(letter_name) as l:
+            l.write(content)
+
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self._name}, birthyear: {self.birthyear})"
+
+class Pupil(HogwartsMember):
+    """
+    Create a Hogwarts Pupil
+    """
+
+    def __init__(self, name: str, birthyear: int, sex: str, house: str, start_year: int, pet: tuple = None):
+        super().__init__(name, birthyear, sex)
+        self.house = house
+        self.start_year = start_year
+        self.known_spells = set()
+
+        if pet is not None:
+            self.pet_name, self.pet_type = pet
+
+        self._owls = {
+                'Study of Ancient Runes': False,
+                'Arithmancy': False,
+                'Astronomy': False,
+                'Care of Magical Creatures': False,
+                'Charms': False,
+                'Defence Against the Dark Arts': False,
+                'Divination': False,
+                'Herbology': False,
+                'History of Magic': False,
+                'Muggle Studies': False,
+                'Potions': False,
+                'Transfiguration': False}
+
+        self._friends = []
+
+    @classmethod
+    def harry(cls):
+        return cls('Harry James Potter', 1980, 'male', 'Griffindor', 1991, ('Hedwig', 'owl'))
+
+
+
+class Letter:
+    total_number_of_letters = 0
+
+    def __init__(self, letter_name):
+        self.letter_name = letter_name
+        self.__class__.total_number_of_letters += 1
+
+    def __enter__(self):
+        self.letter = open(self.letter_name, 'w')
+        return self.letter
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.letter:
+            self.letter.close()
+
+
+if __name__ == "__main__":
+    harry = Pupil.harry()
+
+    letter_content = "Hi Hagrid! \nCan Ron, Hermione and I stop by for a tea this afternoon? \nHarry"
+    harry.write_letter('Hagrid', letter_content)
+
+    print(f"Total number of letter creates so far: {Letter.total_number_of_letters}")
+

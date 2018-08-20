02e4c174736393bf0b5a3d43971db42b689a4b97
zotroneneis <popkes@gmx.net>
2018-08-19 17:26:24 -0700

    magical universe updated

--- test_code/test_castle_kilmere_member_class.py
+++ test_code/test_castle_kilmere_member_class.py
@@ -1,60 +1,60 @@
 import pytest
-from harry_potter_universe.harry_potter_universe import HogwartsMember
+from cleon_potter_universe.cleon_potter_universe import CastleKilmereMember
 
 @pytest.fixture
-def hagrid():
-    hagrid = HogwartsMember('Rubeus Hagrid', 1928, 'male')
-    return hagrid
+def bromley():
+    bromley = CastleKilmereMember('Bromley Huckabee', 1956, 'male')
+    return bromley
 
 @pytest.fixture
-def hagrid_with_traits():
-    hagrid = HogwartsMember('Rubeus Hagrid', 1928, 'male')
-    hagrid.add_trait('kind')
-    hagrid.add_trait('wild')
-    hagrid.add_trait('mean', False)
-    return hagrid
+def bromley_with_traits():
+    bromley = CastleKilmereMember('Bromley Huckabee', 1956, 'male')
+    bromley.add_trait('kind')
+    bromley.add_trait('wild')
+    bromley.add_trait('mean', False)
+    return bromley
 
-def test_correctness_of_attributes_(hagrid):
-    assert hagrid._name == 'Rubeus Hagrid'
-    assert hagrid.birthyear == 1928
-    assert hagrid.sex == 'male'
+def test_correctness_of_attributes_(bromley):
+    assert bromley._name == 'Bromley Huckabee'
+    assert bromley.birthyear == 1956
+    assert bromley.sex == 'male'
 
-def test_add_positive_traits(hagrid):
-    hagrid.add_trait('kind')
-    hagrid.add_trait('wild')
-    assert hagrid._traits == {'kind': True, 'wild': True}
+def test_add_positive_traits(bromley):
+    bromley.add_trait('kind')
+    bromley.add_trait('wild')
+    assert bromley._traits == {'kind': True, 'wild': True}
 
-def test_add_negative_trait(hagrid):
-    hagrid.add_trait('mean', False)
-    assert hagrid._traits == {'mean': False}
+def test_add_negative_trait(bromley):
+    bromley.add_trait('mean', False)
+    assert bromley._traits == {'mean': False}
 
-def test_exhibit_traits(hagrid_with_traits):
-    assert hagrid_with_traits.exhibits_trait('kind') == True
-    assert hagrid_with_traits.exhibits_trait('mean') == False
-    assert hagrid_with_traits.exhibits_trait('smart') == None
+def test_exhibit_traits(bromley_with_traits):
+    assert bromley_with_traits.exhibits_trait('kind') == True
+    assert bromley_with_traits.exhibits_trait('mean') == False
+    assert bromley_with_traits.exhibits_trait('smart') == None
 
-def test_print_traits(capfd, hagrid_with_traits):
-    hagrid_with_traits.print_traits()
+def test_print_traits(capfd, bromley_with_traits):
+    bromley_with_traits.print_traits()
     stdout, err = capfd.readouterr()
     stdout = stdout.strip()
-    assert stdout == 'Rubeus Hagrid is kind, wild but not mean'
+    assert stdout == 'Bromley Huckabee is kind, wild but not mean'
 
 def test_init_raises_exception_with_missing_arguments():
     with pytest.raises(TypeError):
-        hagrid = HogwartsMember()
+        bromley = CastleKilmereMember()
 
-def test_says(hagrid):
-    assert hagrid.says("Hi Harry!") == "Rubeus Hagrid says: Hi Harry!"
+def test_says(bromley):
+    assert bromley.says("Hi Cleon!") == "Bromley Huckabee says: Hi Cleon!"
 
-def test_name_property(hagrid):
-    assert hagrid.name == 'Rubeus Hagrid'
+def test_name_property(bromley):
+    assert bromley.name == 'Bromley Huckabee'
 
-def test_age_property(hagrid):
-    assert hagrid.age == 90 # this holds only if the current year is 2018!
+def test_age_property(bromley):
+    assert bromley.age == 90 # this holds only if the current year is 2018!
 
-def test_repr_output(capfd, hagrid):
-    print(hagrid)
+def test_repr_output(capfd, bromley):
+    print(bromley)
     stdout, err = capfd.readouterr()
     stdout = stdout.strip()
-    assert stdout == 'HogwartsMember(Rubeus Hagrid, birthyear: 1928)'
+    assert stdout == 'CastleKilmereMember(Bromley Huckabee, birthyear: 1956)'
 

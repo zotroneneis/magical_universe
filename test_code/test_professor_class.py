e540cfd7c114962d07d10fbc80be8777db5f4969
zotroneneis <popkes@gmx.net>
2018-08-23 09:43:28 -0700

    further test code added

---
+++ test_code/test_professor_class.py
@@ -0,0 +1,20 @@
+import pytest
+import datetime
+from magical_universe.magical_universe import Professor
+
+now = datetime.datetime.now().year
+
+@pytest.fixture
+def mirren():
+    return Professor.mirren()
+
+def test_correctness_of_attributes_(mirren):
+    assert mirren.name == "Miranda Mirren"
+    assert mirren.house == 'House of Courage'
+    assert mirren.subject == 'Transfiguration'
+
+def test_repr_output(capfd, mirren):
+    print(mirren)
+    stdout, err = capfd.readouterr()
+    stdout = stdout.strip()
+    assert stdout == 'Professor(Miranda Mirren, birthyear: 1963, subject: Transfiguration)'

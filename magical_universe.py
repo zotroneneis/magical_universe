b0431b01e5f4f8f03e66f39a33cbac91c721dc00
zotroneneis <popkes@gmx.net>
2018-08-29 10:07:53 +0200

    new classmethods for Spell types added

--- magical_universe.py
+++ magical_universe.py
@@ -314,10 +314,11 @@ class Pupil(CastleKilmereMember):
 
 class Spell(metaclass=ABCMeta):
     """Creates a spell"""
-    def __init__(self, name: str, incantation: str, effect: str, min_year: int = None):
+    def __init__(self, name: str, incantation: str, effect: str, difficulty: str, min_year: int = None):
         self.name = name
         self.incantation = incantation
         self.effect = effect
+        self.difficulty = difficulty
         self.min_year = min_year
 
     @abstractmethod
@@ -338,9 +339,10 @@ class Charm(Spell):
     Creates a charm  -
     a spell that alters the inherent qualities of an object
     """
-    def __init__(self, name: str, incantation: str, effect: str, difficulty: str = None, min_year: int = None):
-        super().__init__(name, incantation, effect, min_year)
-        self.difficulty = difficulty
+    def __init__(self, name: str, incantation: str, effect: str,
+                 difficulty: str, min_year: int = None):
+
+        super().__init__(name, incantation, effect, difficulty, min_year)
 
     @property
     def defining_feature(self):
@@ -348,35 +350,46 @@ class Charm(Spell):
                 "that is, its behaviour and capabilities")
 
     def cast(self):
-        print(f"{self.incantation}!")
+        return(f"{self.incantation}!")
 
     @classmethod
     def stuporus_ratiato(cls):
         return cls('Stuporus Ratiato', 'Stuporus Ratiato', 'Makes objects fly', 'simple', 1)
 
+    @classmethod
+    def liberula(cls):
+        return cls('Liberula', 'Liberula', 'Allows a person to breath under water', 'difficult', 5)
 
 class Transfiguration(Spell):
     """
     Creates a transfiguration -
     a spell that alters the form or appearance of an object
     """
-    def __init__(self, name: str, incantation: str, effect: str):
-        super().__init__(name, incantation, effect)
+    def __init__(self, name: str, incantation: str, effect: str,
+                 difficulty: str, min_year: int = None):
+
+        super().__init__(name, incantation, effect, difficulty, min_year)
 
     @property
     def defining_feature(self):
         return "Alteration of the object's form or appearance"
 
+    @classmethod
+    def alteraror_canieo(cls):
+        return cls('Alteraro Canieo', 'Alteraro Canieo', 'Turns an object into a can', 'simple', 2)
+
     def cast(self):
-        pass
+        return(f"{self.incantation}!")
 
 class Jinx(Spell):
     """
     Creates a jinx -
     a spell whose effects are irritating but amusing
     """
-    def __init__(self, name: str, incantation: str, effect: str):
-        super().__init__(name, incantation, effect)
+    def __init__(self, name: str, incantation: str, effect: str,
+                 difficulty: str, min_year: int = None):
+
+        super().__init__(name, incantation, effect, difficulty, min_year)
 
     @property
     def defining_feature(self):
@@ -384,16 +397,22 @@ class Jinx(Spell):
                 "a spell whose effects are irritating but amusing, "
                 "almost playful and of minor inconvenience to the target")
 
+    @classmethod
+    def inceptotis(cls):
+        return cls('Inceptotis', 'Inceptotis', 'Makes a person talk baby talk', 'simple')
+
     def cast(self):
-        pass
+        return(f"{self.incantation}!")
 
 class Hex(Spell):
     """
     Creates a hex -
     a spell that affects an object in a negative manner
     """
-    def __init__(self, name: str, incantation: str, effect: str, min_year: int = None):
-        super().__init__(name, incantation, effect, min_year)
+    def __init__(self, name: str, incantation: str, effect: str,
+                 difficulty: str, min_year: int = None):
+
+        super().__init__(name, incantation, effect, difficulty, min_year)
 
     @property
     def defining_feature(self):
@@ -401,54 +420,79 @@ class Hex(Spell):
                 "Affects an object in a negative manner. "
                 "Major inconvenience to the target.")
 
+    @classmethod
+    def rectaro(cls):
+        return cls('Rectaro', 'Rectaro', 'Exchanges a persons arms and legs', 'difficult')
+
     def cast(self):
-        pass
+        return(f"{self.incantation}!")
 
 class Curse(Spell):
     """
     Creates a curse -
     a spell that affects an object in a stflynngly negative manner
     """
-    def __init__(self, name: str, incantation: str, effect: str, difficulty: str = None):
-        super().__init__(name, incantation, effect)
+    def __init__(self, name: str, incantation: str, effect: str,
+                 difficulty: str, min_year: int = None):
+
+        super().__init__(name, incantation, effect, difficulty, min_year)
 
     @property
     def defining_feature(self):
         return ("Worst kind of dark magic - "
-                "Intended to affect an object in a stflynngly negative manner.")
+                "Intended to affect an object in a strongly negative manner.")
+
+    @classmethod
+    def fiera_satanotis(cls):
+        return cls('Torture spell', 'Fiera Satanotis',
+                   'Tortures a person, makes person suffer deeply', 'difficult')
 
     def cast(self):
-        pass
+        return(f"{self.incantation}!")
 
 class CounterSpell(Spell):
     """
     Creates a counter-spell -
     a spell that inhibits the effect of another spell
     """
-    def __init__(self, name: str, incantation: str, effect: str):
-        super().__init__(name, incantation, effect)
+    def __init__(self, name: str, incantation: str, effect: str,
+                 difficulty: str, min_year: int = None):
+
+        super().__init__(name, incantation, effect, difficulty, min_year)
 
     @property
     def defining_feature(self):
         return ("Inhibites the effects of another spell")
 
+    @classmethod
+    def mufindo_immolim(cls):
+        return cls('Mufindo Immolim', 'Mufindo Immolim',
+                   'Counteracts the immobilisation spell that prevents a person from moving', 'simple')
+
     def cast(self):
-        pass
+        return(f"{self.incantation}!")
 
 class HealingSpell(Spell):
     """
     Creates a healing-spell -
     a spell that improves the condition of a living object
     """
-    def __init__(self, name: str, incantation: str, effect: str):
-        super().__init__(name, incantation, effect)
+    def __init__(self, name: str, incantation: str, effect: str,
+                 difficulty: str, min_year: int = None):
+
+        super().__init__(name, incantation, effect, difficulty, min_year)
 
     @property
     def defining_feature(self):
         return "Improves the condition of a living object"
 
+    @classmethod
+    def porim_perfite(cls):
+        return cls('Wound healing spell', 'Porim Perfite',
+                   'Heals all kinds of wounds, even bad ones', 'difficult')
+
     def cast(self):
-        pass
+        return(f"{self.incantation}!")
 
 
 @dataclass(frozen=True)
@@ -463,7 +507,7 @@ class DarkArmyMember():
         return lord_odon
 
     def cast_spell(self, spell):
-        print(f"{self.name}: {spell.incantation}!")
+        return(f"{self.name}: {spell.incantation}!")
 
 
 @dataclass
@@ -516,11 +560,23 @@ class Potion:
 
 if __name__ == "__main__":
 
-    flask_of_remembrance = Potion(['raven eggshells', 'tincture of thyme', 'unicorn tears',
-                                    'dried onions', 'powdered ginger root'])
+    charm = Charm.liberula()
+    print('charm: ', charm)
 
-    vial_of_anger = Potion(['dried dragon skin', 'leeches', 'shredded elephant tusk',
-                            'horned flies', 'earthworm juice'])
+    transfiguration = Transfiguration.alteraror_canieo()
+    print(transfiguration.cast())
 
-    for ingredient in flask_of_remembrance:
-        print(ingredient)
+    jinx = Jinx.inceptotis()
+    print('jinx: ', jinx)
+
+    hex_ = Hex.rectaro()
+    print(hex_.cast())
+
+    curse = Curse.fiera_satanotis()
+    print('curse: ', curse)
+
+    healing_spell = HealingSpell.porim_perfite()
+    print(healing_spell.cast())
+
+    counter_spell = CounterSpell.mufindo_immolim()
+    print(counter_spell.cast())

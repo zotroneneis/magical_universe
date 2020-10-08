2e9a67eb03f942df1551ca24f953fe46461b0e32
Anna-Lena Popkes <popkes@gmx.net>
2020-10-08 09:46:40 +0200

    Code for day 1 updated

--- code_per_day/day_1.py
+++ code_per_day/day_1.py
@@ -8,24 +8,19 @@ Link to blog post with explanations: http://www.alpopkes.com/posts/2018/07/codin
 """
 
 class CastleKilmereMember:
-    """
-    Creates a member of the Castle Kilmere School of Magic
-    """
+    """ Creates a member of the Castle Kilmere School of Magic """
 
     def __init__(self, name, birthyear, sex):
-        self._name = name
+        self.name = name
         self.birthyear = birthyear
         self.sex = sex
 
     def says(self, words):
-        return f"{self._name} says {words}"
+        return f"{self.name} says {words}"
 
 
 class Pupil(CastleKilmereMember):
-    """
-    Create a Castle Kilmere Pupil
-    """
-
+    """ Create a Castle Kilmere Pupil """
     def __init__(self, name, birthyear, sex, start_year, pet=None):
         super().__init__(name, birthyear, sex)
         self.start_year = start_year
@@ -34,35 +29,26 @@ class Pupil(CastleKilmereMember):
             self.pet_name, self.pet_type = pet
 
         self._elms = {
+                  'Critical Thinking': False,
+                  'Self-Defense Against Fresh Fruit': False,
                   'Broomstick Flying': False,
-                  'Art': False,
                   'Magical Theory': False,
                   'Foreign Magical Systems': False,
                   'Charms': False,
                   'Defence Against Dark Magic': False,
-                  'Divination': False,
-                  'Herbology': False,
                   'History of Magic': False,
                   'Potions': False,
                   'Transfiguration': False}
 
 class Professor(CastleKilmereMember):
-  """
-  Creates a Castle Kilmere professor
-  """
-
+  """ Creates a Castle Kilmere professor """
   def __init__(self, name, birthyear, sex, subject, department=None):
       super().__init__(name, birthyear, sex)
       self.subject = subject
-      if department is not None:
-          self.department = department
-
+      self.department = department
 
 class Ghost(CastleKilmereMember):
-    """
-    Creates a Castle Kilmere ghost
-    """
-
+    """ Creates a Castle Kilmere ghost """
     def __init__(self, name, birthyear, sex, year_of_death):
         super().__init__(name, birthyear, sex)
         self.year_of_death = year_of_death
@@ -75,4 +61,6 @@ if __name__ == "__main__":
                 birthyear=2008,
                 sex='female',
                 start_year=2020,
-                pet=('Cotton', 'owl'))
+                pet=('Ramses', 'cat'))
+
+  print(lissy.says("Hello Luke!"))
